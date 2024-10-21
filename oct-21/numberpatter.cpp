1897be29b0300098f36d390be496f5c4f4023823
kishan <vishwakarmakishan620@gmail.com>
2024-10-21 12:57:57 +0530

    add

---
+++ oct-21/numberpatter.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+//     for(int i=1 ; i<=3 ;i++){
+//         for(int j=1 ;j<=i ;j++){
+//             cout<<j;
+//         }
+//         cout<<"\n";
+//     }
+// }
+
+// int main(){
+//     for(int i=1 ; i<=3 ;i++){
+//         for(int j=1 ;j<=i ;j++){
+//             cout<<j*j;
+//         }
+//         cout<<"\n";
+//     }
+// }
+
+// int main(){
+//     int count=0;
+//     for(int i=1 ; i<=3 ;i++){
+//         for(int j=1 ;j<=i ;j++){
+//             count++;
+//             cout<<count<<"\t";
+//         }
+//         cout<<"\n";
+//     }
+// }
+
+int main(){
+    int count=0;
+    for(int i=1 ; i<=3 ;i++){
+        for(int j=1 ;j<=i ;j++){
+            count++;
+            cout<<count*count<<"\t";
+        }
+        cout<<"\n";
+    }
+}
