fcf633b8cb491581804df3cc461a83774f715cd7
kishan <vishwakarmakishan620@gmail.com>
2024-12-13 13:31:41 +0530

    add

---
+++ dec-13-oops-start/class.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+// cpp support object oriented programing system concept. it is bottom to top approach.
+// it is having four pillars
+// 1.object & class
+// 2.Encapsulation
+// 3.Abstraction
+// 4.Inheritance
+
+// object: 
+
+int main(){
+
+}
