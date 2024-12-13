cd41a9013efe7b4b515fb6d997eacda83d23430d
kishan <vishwakarmakishan620@gmail.com>
2024-12-13 13:06:30 +0530

    add

--- dec-12/fibonacci-with-recursion.cpp
+++ dec-12/fibonacci-with-recursion.cpp
@@ -2,15 +2,22 @@
 using namespace std;
 
 int fib(int a){
-    if(a<1){
+    int count=0;
+    if(a<0){
+        count++;
+        return 0;
+    }
+    if(a==0){
+        count++;
         return 1;
     }
+    cout<<count;
     return fib(a-1)+fib(a-2);
 }
 
 int main(){
     int a;
-    cout<<"Enter any no for fibonacci\n";
+    cout<<"Enter any no for fibonacci number of that term\n";
     cin>>a;
     cout<<fib(a);
 }
