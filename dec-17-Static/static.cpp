b25b654447502431b355a371f6b7a6b679a165b7
kishan <vishwakarmakishan620@gmail.com>
2024-12-17 13:25:20 +0530

    add

--- dec-17-Static/static.cpp
+++ dec-17-Static/static.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 // static variable are shareabl not copyable
 // static data member and static member function is a attribut of a class. while non-static data member and non-static member function is attribut of an object. 
-// note: whenever we call static member function we need not create an object. 
+// note: whenever we call static member function we need not create an object. we can access with the name of class using scope resolution operator.
+// types of classes.
+// single class: if a program having oly one class is called single class.
+// multi class:if a program having more then one classes is call multiclass.
 static int a=10;
 static int b=80;
 class top{
